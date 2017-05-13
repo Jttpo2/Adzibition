2818e201aac762279e945d8bd299c9ab91e925c1
John Petersson <johnpetersson@gmail.com>
2017-05-13 02:26:16 +0200

    Test plane scales to image targetsize.

--- Assets/Scripts/BackgroundCover.cs
+++ Assets/Scripts/BackgroundCover.cs
@@ -15,39 +15,42 @@ public class BackgroundCover : MonoBehaviour
 		plane = GameObject.CreatePrimitive (PrimitiveType.Plane);
 
 		// Set image target as parent
-		plane.transform.SetParent (thisTransform);
+//		plane.transform.SetParent (thisTransform);
+
+		// Set image targets parent as parent, for correct scaling
+		plane.transform.SetParent (thisTransform.parent);
 
 		// Center on image target
 		plane.transform.position = thisTransform.position;
 
-		// Move in front of target a tiny bit to not come into glitch conflict with image target. Unnecessary?
-		plane.transform.Translate (new Vector3 (0, 0.0001f, 0));
-
-		// Scale to image target size
-		// TODO: Scale to width of image target if wide, else scale to height
-		plane.transform.localScale = new Vector3 (0.1f, 0.1f, 0.1f);
-//			thisTransform.localScale * (0.1f);
-//			new Vector3 (1, 1, 1);
-//		1.8 0.1
-
 		// Rotate it to stand up, and turn it to face the camera (90 degrees z and 180 degrees x)
 		Quaternion standUp = Quaternion.Euler (new Vector3 (0, 90, 0));
 		plane.transform.localRotation = standUp;
+
+		// Scale to image target size
+		sizeToImgTarget ();
+
+		// Move in front of target a tiny bit to not come into glitch conflict with image target. Unnecessary?
+		plane.transform.Translate (new Vector3 (0, -0.1f, 0));
+
+		// TODO: Scale to width of image target if wide, else scale to height
 
 		// Paint it black
 		plane.GetComponent <Renderer> ().material.color = Color.black;
 
 		// Make it plain (apply a basic shader with no lighting)
 		plane.GetComponent <Renderer> ().material.shader = Shader.Find ("Unlit/Color");
-
-		//		Debug.Log (material.color);
-
-
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+
+	private void sizeToImgTarget ()
 	{
-//		plane.transform.localScale = 
+		// Get image targe size 
+		Vector2 imgTargetSize = GetComponent <Vuforia.ImageTargetBehaviour> ().GetSize ();
+		float imgTargetWidth = imgTargetSize.x;
+		float imgTargetHeight = imgTargetSize.y;
+
+		// Scale plane to image target size
+		plane.transform.localScale = new Vector3 (imgTargetHeight / 10f, 0.1f, imgTargetWidth / 10f);
 	}
 }
